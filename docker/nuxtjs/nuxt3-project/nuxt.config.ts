c81a426141683307f137176386ba2da638facac1
Hidenori14 <24432855+Hidenori14@users.noreply.github.com>
2024-06-12 18:24:17 +0900

    Fixed security alart

--- docker/nuxtjs/nuxt3-project/nuxt.config.ts
+++ docker/nuxtjs/nuxt3-project/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  // devtools: { enabled: true },
   ssr: true,
   modules: [
     '@pinia/nuxt',
